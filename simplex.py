2712b6eb3209a68458f5afdf1cfc65343f4222bc
Arthur AM <arthrfrnco@gmail.com>
2020-06-18 15:54:28 -0300

    update README

--- simplex.py
+++ simplex.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from sys import argv
 
 def read_model(arq):
     lines_list = arq.readlines()
@@ -64,25 +64,20 @@ def solve(c, A, b, non_basic, basic, minimum, minimum_2):
 
     while True:
         xB, z = step_one(A, b, c, basic, non_basic)
-        # print(basic)
-        # print(non_basic)
-        # print('xB:', xB)
-        # print('z:', z)
-        # idx eh o index da variavel que entra na base
+
+        # idx is the index of the variable entering the base
+        # w is the certificate
         w, minimum, idx = step_two(A, b, c, basic, non_basic, xB, minimum)
-        # w eh o certificado
-        # print('w:', w)
-        # print('minimum:', minimum)
-        # print('idx:', idx)
+
         if minimum >= 0:
-            status = "otima"
+            status = "optimal"
             break
         else:
             minimum = float('inf')
         y = step_three(A, idx, basic)
 
         if (y <= 0).all():
-            status = "ilimitada"
+            status = "unlimited"
             break
 
         leaving_var = step_four(A, b, y, basic, minimum_2)
@@ -93,8 +88,6 @@ def solve(c, A, b, non_basic, basic, minimum, minimum_2):
         non_basic.append(leaving_var)
         basic.sort()
         non_basic.sort()
-        # print('basic:', basic)
-        # print('non_basic:', non_basic)
 
     return z, w, status, xB
 
@@ -102,14 +95,14 @@ def solve(c, A, b, non_basic, basic, minimum, minimum_2):
 def main():
     minimum = float('inf')
     minimum_2 = float('inf')
-    arq = open("teste.txt", "r")
+    arq = open(argv[1], "r")
     model, n_cols, n_rows = read_model(arq)
     c, A, b, non_basic, basic = create_model(model, n_cols, n_rows)
     first_basic = basic
-    # first_n_basic = non_basic
-    # aqui criamos uma PL auxiliar
-    # SE OBJ = 0, CONTINUO NORMALMENTE
-    # SE OBJ != 0, PL ORIGINAL EH INVIAVEL
+
+    # here we build and auxiliary problem to check feasibility
+    # if the objective value is 0, continue
+    # else, the original LP is infeasible
     if (b < 0).any():
         negative_idx = np.where(b < 0)
         negative_idx = [i for i in negative_idx[0]]
@@ -120,34 +113,34 @@ def main():
         A2[negative_idx] = -1.0*A2[negative_idx]
         b2[negative_idx] = -1.0*b2[negative_idx]
         A2 = np.append(A2, np.identity(n_rows), axis=1)
-        # for i in range(A2.shape[0]):
-        #   c2 = np.add(c2, A2[i])
         non_basic2 = [i for i in range(n_cols + n_rows)]
         basic2 = [i+n_rows+n_cols for i in range(n_rows)]
         z, w, status, xB = solve(c2, A2, b2, non_basic2, basic2, minimum, minimum_2)
 
-        # se a PL auxiliar tem obj < 0, acabou
+        # if the auxiliary problem has objective < 0, it's over
+        # our original problem is infeasible
         if z < 0:
-            status = 'inviavel'
+            status = "infeasible"
 
-        # se nao, rodamos o simplex
+        # if the auxiliary problem has objective >=0, we solve the
+        # original problem with this base
         else:
             z, w, status, xB = solve(c, A, b, non_basic, basic, minimum, minimum_2)
 
     else:
         z, w, status, xB = solve(c, A, b, non_basic, basic, minimum, minimum_2)
 
-    solucao = [0. for i in range(n_cols + n_rows)]
-    solucao = {key: value for (key, value) in zip(basic, xB)}
+    solution = [0. for i in range(n_cols + n_rows)]
+    solution = {key: value for (key, value) in zip(basic, xB)}
 
     x = [0. for i in range(n_rows+n_cols)]
 
-    for key in solucao:
-        x[key] = solucao[key]
+    for key in solution:
+        x[key] = solution[key]
 
     # x = x[:n_cols]
 
-    if status == 'ilimitada':
+    if status == "unlimited":
         w = np.zeros(n_cols+n_rows)
         aux = np.where((A[1:] < 0).all(axis=0))
         w[aux[0][0]] = 1
@@ -156,19 +149,20 @@ def main():
 
     w = w.tolist()
 
-    if status == 'otima':
+    if status == "optimal":
         print(status)
         print(z)
         print(*x)
         print(*w)
-    elif status == 'ilimitada':
+    elif status == "unlimited":
         print(status)
         print(*x)
         print(*w)
-    elif status == 'inviavel':
+    elif status == "infeasible":
         print(status)
-        u = [-1.0*v for v in w]        
+        u = [-1.0*v for v in w]
         print(*u)
+
 
 
 if __name__ == "__main__":
